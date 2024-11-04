46ff04f599f3d18d8f6dd90baefb178166e19e58
ad5322 <arundeepak966@gmail.com>
2024-11-04 23:08:52 +0530

    foreign key

--- django_orm/hr/models.py
+++ django_orm/hr/models.py
@@ -1,8 +1,27 @@
 from django.db import models
 
+
+class Contact(models.Model):
+    phone = models.CharField(max_length=50,unique=True,null=True)
+    address = models.CharField(max_length=50,null=True)
+    
+    def __str__(self):
+        return self.phone
+
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True,blank=True)
+    
+    def __str__(self):
+        return self.name
+
 class Employee(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100,null=True)
+    last_name = models.CharField(max_length=100,null=True)
+    
+    contact = models.OneToOneField(Contact,on_delete=models.CASCADE,null=True)
+    
+    department = models.ForeignKey(Department,on_delete=models.CASCADE,null=True)
     
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
